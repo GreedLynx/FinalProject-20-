932cbedcf10a627d1fee416b9b0183738335fc65
nopalppt <naufalshofil1603@gmail.com>
2025-09-30 14:59:11 +0700

    Update: Memperbaiki logika input dan menambahkan fungsi cetak hasil.

--- test.java
+++ test.java
@@ -1,5 +1,11 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hai kakak!");
+        String [] nama = {"Farel", "Rizpol", "Tegar SadBoy"};
+        cetakNama(nama);
+    }
+    static void cetakNama(String [] daftar){
+        for (String daftar1 : daftar) {
+            System.out.println(daftar1);
+        }
     }
 }
